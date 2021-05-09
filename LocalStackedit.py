bfe6f61428116db53b0368297a772f8d26f4a565
Aymeric Deschard <aymeric.deschard@disroot.org>
2021-05-09 09:15:35 +0200

    Move webdriver interactions to its own module Browser

--- LocalStackedit.py
+++ LocalStackedit.py
@@ -8,106 +8,23 @@ import pickle
 from urllib3.exceptions import MaxRetryError
 
 try:
-	from selenium import webdriver
-	from selenium.webdriver.common.keys import Keys
-	from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, WebDriverException
-	from selenium.webdriver.remote.command import Command
-	from selenium.webdriver.common.keys import Keys
+	# from selenium import webdriver
+	# from selenium.webdriver.common.keys import Keys
+	# from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, WebDriverException
+	# from selenium.webdriver.remote.command import Command
+	# from selenium.webdriver.common.keys import Keys
 	from pynput import keyboard
 except ModuleNotFoundError as e:
 	print(f"ERROR - Missing dependency: {e}.\nRun:\npip install -r requirements.txt")
 	exit(1)
+
+from src import Browser
 
 # ==============================================================================
 #                                     SETUP
 # ==============================================================================
 
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
-COOKIES_FILE = os.path.join(SCRIPT_PATH, "cookies.pkl")
-
-
-if platform.system() == "Linux":
-	WEBDRIVER = os.path.join(SCRIPT_PATH, "Webdrivers", "chromedriver_linux64")
-	BRAVE_PATH = subprocess.check_output(["which", "brave-browser"]).decode("utf-8")
-else:
-	raise NotImplementedError(f"ERROR - This script is not compatible with {platform.system()}")
-	exit(1)
-
-# For some reason, trailing spaces are added to the output
-BRAVE_PATH = BRAVE_PATH.strip()
-print("INFO - Brave binary path: ", BRAVE_PATH)
-OPTION = webdriver.ChromeOptions()
-OPTION.binary_location = BRAVE_PATH
-
-# Directory for brave's user data
-DATA_DIR = os.path.join(SCRIPT_PATH, "BraveData")
-OPTION.add_argument("--user-data-dir=" + DATA_DIR)
-if not os.path.exists(DATA_DIR):
-	IS_FIRST_LAUNCH = True
-	os.mkdir(DATA_DIR) 
-	print(f"INFO - User data directory created: {DATA_DIR}")
-else:
-	print(f"INFO - Reusing prexisting user data directory: {DATA_DIR}")
-
-# No popup for saving downloads
-OPTION.add_argument("profile.default_content_settings.popups=0")
-
-# ==============================================================================
-#                                BROWSER ACTIONS
-# ==============================================================================
-def new_tab() -> None:
-	global browser
-	browser.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 't')
-	browser.find_element_by_tag_name("body").send_keys(Keys.COMMAND + 't')
-	return
-
-
-def wait_for_element(element2Find: str) -> None:
-	global browser
-	try:
-		browser.find_element_by_class_name(element2Find)
-	except NoSuchElementException:
-		isFound = False
-	else: isFound = True
-
-	while not isFound:
-		try:
-			browser.find_element_by_class_name(element2Find)
-		except NoSuchElementException:
-			isFound = False
-			sleep(.25)
-		else:
-			isFound = True
-
-	print(f"INFO - Page loaded with element of class: {element2Find}.")
-	return
-
-
-
-def is_alive() -> bool:
-	global browser
-	try:
-		browser.current_url
-	except (MaxRetryError, WebDriverException):
-		return False 
-	else:
-		return True
-
-
-def on_close() -> None:
-	global browser
-	global h
-	# backup cookies
-	if is_alive():
-		with open(COOKIES_FILE, "wb") as f:
-			pickle.dump(browser.get_cookies(), f)
-		print("INFO - Cookies saved.")
-		browser.close()
-	else:
-		print("WARN - Browser is dead. Cookies unsaved.")
-	h.stop()
-	print("INFO - Bye.")
-
 
 # ==============================================================================
 #                                      MAIN
@@ -122,30 +39,10 @@ if __name__ == "__main__":
 	docFile = os.path.realpath((sys.argv[1]))
 
 	try:
-		# Starts the browser
-		try:
-			browser = webdriver.Chrome(
-					executable_path = WEBDRIVER,
-					options = OPTION
-				)
-		except Exception as e:
-			print(e)
-			print("ERROR - It's seem an instance of brave is already running. Only one instance allowed.")
-			exit(1)
-		# Loads cookies
-		if os.path.exists(COOKIES_FILE):
-			print("INFO - Loading cookies...")
-			with open(COOKIES_FILE, "rb") as f:
-				cookies = pickle.load(f)
-			for c in cookies:
-				browser.add_cookies(c)
-			browser.refresh()
-		else:
-			print("INFO - No cookies file")
-
+		browser = Browser.initialize(SCRIPT_PATH)
 		# Open stakedit page
 		browser.get("https://stackedit.io/app#")
-		wait_for_element("editor")
+		Browser.wait_for_element("editor", browser)
 
 		# Get stackedit document title
 		titleField = browser.find_elements_by_tag_name("input")[0]
@@ -225,8 +122,10 @@ if __name__ == "__main__":
 
 		def on_quit_shortcut():
 			global windowName
+			global h
+			global browser
 			if get_focused_window() == windowName:
-				on_close()
+				Browser.on_close(SCRIPT_PATH, h, browser)
 			else:
 				print(f"INFO - Quit shortcut pressed, but the window {get_focused_window()} is not the proper one {windowName}")
 
@@ -237,12 +136,6 @@ if __name__ == "__main__":
 		}) as h:
 			h.join()
 
-		# TODO Save on CTRL+S
-		# Menu button: <button> tour-step-anchor menu
-		#	<div> inner     Import/export
-		#		<div> menu-entry__text flex flex--column
-		#			<div>Export as Markdown
-
 
 	except KeyboardInterrupt:
-		on_close()	
+		on_close(SCRIPT_PATH, h, browser)	
---
+++ src/Browser.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""Browser handles all the interaction with the Selenium driver"""
+# std libs
+import os
+import platform
+import subprocess
+import pickle
+from time import sleep
+
+# dependencies
+try:
+	from selenium import webdriver
+	from selenium.common.exceptions import NoSuchElementException
+except ModuleNotFoundError as e:
+	print(f"ERROR - Missing dependency: {e}.\nRun:\npip install -r requirements.txt")
+	exit(1)
+
+
+
+def new_tab(browser: object) -> None:
+	"""Open a new tab"""
+	browser.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 't')
+	browser.find_element_by_tag_name("body").send_keys(Keys.COMMAND + 't')
+	return
+
+
+def wait_for_element(element2Find: str, browser: object) -> None:
+	"""Alt the execution until an element2find by classname is on the page."""
+	try:
+		browser.find_element_by_class_name(element2Find)
+	except NoSuchElementException:
+		isFound = False
+	else: isFound = True
+
+	while not isFound:
+		try:
+			browser.find_element_by_class_name(element2Find)
+		except NoSuchElementException:
+			isFound = False
+			sleep(.25)
+		else:
+			isFound = True
+
+	print(f"INFO - Page loaded with element of class: {element2Find}.")
+	return
+
+
+def is_alive(browser: object) -> bool:
+	"""Check if an instance of webdriver is still being executed."""
+	try:
+		browser.current_url
+	except (MaxRetryError, WebDriverException):
+		return False 
+	else:
+		return True
+
+
+def on_close(scriptPath: str, hotkeyHandler: object, browser: object) -> None:
+	"""Actions to perform when the user request closing the browser."""
+	# backup cookies
+	if is_alive(browser):
+		cookiesFile = os.path.join(scriptPath, "cookies.pkl")
+		with open(cookiesFile, "wb") as f:
+			pickle.dump(browser.get_cookies(), f)
+		print("INFO - Cookies saved.")
+		browser.close()
+	else:
+		print("WARN - Browser is dead. Cookies unsaved.")
+	hotkeyHandler.stop()
+	print("INFO - Bye.")
+
+
+def initialize(scriptPath: str) -> object:
+	"""Launch a new instance of Selenium webdriver."""
+
+	cookiesFile = os.path.join(scriptPath, "cookies.pkl")
+
+	if platform.system() == "Linux":
+		WEBDRIVER = os.path.join(scriptPath, "Webdrivers", "chromedriver_linux64")
+		BRAVE_PATH = subprocess.check_output(["which", "brave-browser"]).decode("utf-8")
+	else:
+		raise NotImplementedError(f"ERROR - This script is not compatible with {platform.system()}")
+		exit(1)
+
+	# For some reason, trailing spaces are added to the output
+	BRAVE_PATH = BRAVE_PATH.strip()
+	print("INFO - Brave binary path: ", BRAVE_PATH)
+	OPTION = webdriver.ChromeOptions()
+	OPTION.binary_location = BRAVE_PATH
+
+	# Directory for brave's user data
+	DATA_DIR = os.path.join(scriptPath, "BraveData")
+	OPTION.add_argument("--user-data-dir=" + DATA_DIR)
+	if not os.path.exists(DATA_DIR):
+		os.mkdir(DATA_DIR) 
+		print(f"INFO - User data directory created: {DATA_DIR}")
+	else:
+		print(f"INFO - Reusing prexisting user data directory: {DATA_DIR}")
+
+		# Starts the browser
+		try:
+			browser = webdriver.Chrome(
+					executable_path = WEBDRIVER,
+					options = OPTION
+				)
+		except Exception as e:
+			print(f"ERROR - It's seem an instance of brave is already running. Only one instance allowed: {e}")
+			exit(1)
+
+		# Loads cookies
+		if os.path.exists(cookiesFile):
+			print(f"INFO - Loading cookies from {cookiesFile}")
+			with open(cookiesFile, "rb") as f:
+				cookies = pickle.load(f)
+			for c in cookies:
+				browser.add_cookies(c)
+			browser.refresh()
+		else:
+			print("INFO - No cookies file")
+
+	return browser
